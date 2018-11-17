28df8388285352f4dee09b845c635bbbc31f3221
pamarquesit <PedroAntonioMarques@gmail.com>
2018-11-17 17:34:16 +0000

    added spring data jpa services for Speciality. Closes #44

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/SpecialtyRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/SpecialtyRepository.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.repositories;
 
+import guru.springframework.sfgpetclinic.model.Speciality;
 import org.springframework.data.repository.CrudRepository;
 
-public interface SpecialtyRepository extends CrudRepository<SpecialtyRepository, Long> {
+public interface SpecialtyRepository extends CrudRepository<Speciality, Long> {
 }
